e19ce8133d227d9820e83235c6c9992eb1d92805
Yücel KURT <yclkrt4434@gmail.com>
2026-09-13 11:43:05 +0300

    uygulamanın apk dosyası oluşturulabilmesi için gereken düzenlemeler yapıldı.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,10 +7,11 @@ plugins {
 
 android {
     namespace = "com.example.sporlab"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -41,4 +42,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
